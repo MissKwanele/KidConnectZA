e4e4b4cb1203ae56a04411204df6f70558b3e47d
Nonku <ntshele.nonkululeko@gmail.com>
2025-08-07 13:33:54 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -186,7 +186,7 @@ with tab1:
             for row in data:
                 if class_selected != "All Classes" and row.get("Class") != class_selected:
                     continue
-                name = row.get("Name", "Parent")
+                name = row.get("Name", "")
                 number = str(row.get("PhoneNumber", "")).strip()
 
                 # Check for number format and whitelist
